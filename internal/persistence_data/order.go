05d60f0a2a0d8d90ecd4d1cabe7832527d309a60
Grifree <1149290648@qq.com>
2021-02-15 17:39:03 +0800

    m order

--- internal/persistence_data/order.go
+++ internal/persistence_data/order.go
@@ -1,3 +1,35 @@
 package pd
 
+import sq "github.com/goclub/sql"
+
 type IDOrder string
+func (id IDOrder) String() string { return string(id) }
+type TableOrder struct {
+	sq.SoftDeleteDeletedAt
+}
+func (TableOrder) TableName() string { return "order" }
+func (TableOrder) Column() (col struct{
+	ID sq.Column
+	ConsumerID sq.Column
+	GoodsID sq.Column
+	Status sq.Column
+	CreatedAt sq.Column
+	UpdatedAt sq.Column
+}){
+	col.ID = "id"
+	col.ConsumerID = "consumer_id"
+	col.GoodsID = "goods_id"
+	col.Status = "status"
+	col.CreatedAt = "created_at"
+	col.UpdatedAt = "updated_at"
+	return
+}
+
+type Order struct {
+	ID IDOrder `db:"id"`
+	ConsumerID IDConsumer `db:"consumer_id"`
+	GoodsID IDGoods `db:"goods_id"`
+	Status OrderStatus `db:"status"`
+	sq.CreatedAtUpdatedAt
+}
+type OrderStatus string
